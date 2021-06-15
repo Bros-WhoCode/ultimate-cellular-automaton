7cc437e8d807e93bbe2384960bd4d25ce252ce2d
tgashwinkumar <tgashwinkumar@gmail.com>
2021-06-15 20:56:18 +0530

    Made the editor

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import '../styles/App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Input from './Input'
 import Home from './Home';
---
+++ src/utils/format.md
@@ -0,0 +1,7 @@
+## Ruleset
+ARRAY :
+### idx: int
+### array: 2d array (3x3)
+## isvalid: 1/0
+--> array element => val: 1/0, array_idx: int
+### return value : 1/0
