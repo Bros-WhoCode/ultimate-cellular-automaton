03c7e3ddea53888122c99f0deef161b330e4370b
MrRhuezzler <anonymouspyro369@gmail.com>
2021-06-15 16:52:06 +0530

    Basic Structure

---
+++ src/components/Cell.js
@@ -0,0 +1,11 @@
+import '../styles/Cell.css';
+
+const Cell = () => {
+    return (
+        <div className="cell-container">
+            <p>Cell</p>
+        </div>
+    );
+}
+ 
+export default Cell;
---
+++ src/components/World.js
@@ -0,0 +1,11 @@
+import '../styles/World.css';
+
+const World = () => {
+    return (
+        <div className="world-container">
+            <h1>World</h1>
+        </div>
+    );
+}
+ 
+export default World;
