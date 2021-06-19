8771aebd4c50564c5013dfa5e837df4bf26f3817
MrRhuezzler <anonymouspyro369@gmail.com>
2021-06-19 21:36:58 +0530

    Some Changes

--- src/pages/App.js
+++ src/pages/App.js
@@ -11,7 +11,7 @@ const App = () => {
           <Route path='/input'>
             <Input/>
           </Route>
-          <Route exact path='/'>
+          <Route path='/'>
             <Home/>
           </Route>
         </Switch>
