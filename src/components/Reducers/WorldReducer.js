bc6e6c776e02b1840bde72b782645b2aeaee3e3b
MrRhuezzler <anonymouspyro369@gmail.com>
2021-06-23 22:05:30 +0530

    Code Refractoring

--- src/components/Reducers/WorldReducer.js
+++ src/components/Reducers/WorldReducer.js
@@ -9,14 +9,13 @@ class State {
             this.cellSize = 20;
             this.cellSizeChange = 2;
 
-            let cols = Math.floor((convertVwToPx(80) - this.cellSize) / this.cellSize);
-            let rows = Math.floor((convertVhToPx(80) - this.cellSize) / this.cellSize);
+            let maxDim = this.calculateRowsCols();
 
-            this.rows = rows;
-            this.cols = cols;
+            this.rows = maxDim.rows;
+            this.cols = maxDim.cols;
             
-            this.maxRows = rows;
-            this.maxCols = cols;
+            this.maxRows = maxDim.rows;
+            this.maxCols = maxDim.cols;
 
             this.minRows = 10;
             this.minCols = 10;
@@ -25,26 +24,21 @@ class State {
             this.prevMaxCols = [this.minCols];
 
             this.currentState = 0;
-            
 
             this.neighbors = {};
 
-            this.cells = new Array(rows);
+            this.cells = make2DArray(this.rows, this.cols);
 
-            for(let i = 0; i < rows; i++){
-                this.cells[i] = new Array(cols);
-            }
-
-            for(let i = 0; i < rows; i++){
+            for(let i = 0; i < this.rows; i++){
                 this.neighbors[i] = {};
-                for(let j = 0; j < cols; j++){
+                for(let j = 0; j < this.cols; j++){
                     this.cells[i][j] = 0;
 
                     this.neighbors[i][j] = [];
                     for (let k = -1; k < 2; k++) {
                         for (let l = -1; l < 2; l++) {
-                            let row = (k + i + rows) % rows;
-                            let col = (l + j + cols) % cols;
+                            let row = (k + i + this.rows) % this.rows;
+                            let col = (l + j + this.cols) % this.cols;
                             this.neighbors[i][j].push({x : row, y : col});
                         }
                     }
@@ -76,6 +70,13 @@ class State {
 
     }
 
+    calculateRowsCols(){
+        return {
+            rows : Math.floor((convertVhToPx(80) - (this.cellSize * 4)) / this.cellSize),
+            cols : Math.floor((convertVwToPx(80) - (this.cellSize * 4)) / this.cellSize)
+        }
+    }
+
     recalculate({increase, rows, cols}){
 
         console.log(this);
@@ -89,50 +90,47 @@ class State {
 
                 this.cellSize -= this.cellSizeChange;
 
-                let m_cols = Math.floor((convertVwToPx(80) - this.cellSize) / this.cellSize);
-                let m_rows = Math.floor((convertVhToPx(80) - this.cellSize) / this.cellSize);
-
-                this.maxRows = m_rows;
-                this.maxCols = m_cols;
-
             }
         
         }else{
 
             if(rows < this.prevMaxRows[this.prevMaxRows.length - 1] || cols < this.prevMaxCols[this.prevMaxCols.length - 1]){
 
+                if(rows > this.prevMaxRows[this.prevMaxRows.length - 1]){
+                    rows = this.prevMaxRows[this.prevMaxRows.length - 1];
+                }
+
+                if(cols > this.prevMaxCols[this.prevMaxCols.length - 1]){
+                    cols = this.prevMaxCols[this.prevMaxCols.length - 1];
+                }
+
                 this.prevMaxRows.pop();
                 this.prevMaxCols.pop();
 
                 this.cellSize += this.cellSizeChange;
 
-                let m_cols = Math.floor((convertVwToPx(80) - this.cellSize) / this.cellSize);
-                let m_rows = Math.floor((convertVhToPx(80) - this.cellSize) / this.cellSize);
-
-                this.maxRows = m_rows;
-                this.maxCols = m_cols;
-
             }
 
         }
 
+        let maxDim = this.calculateRowsCols();
+
+        this.maxRows = maxDim.rows;
+        this.maxCols = maxDim.cols;
+
         this.rows = rows;
         this.cols = cols;
 
-        this.cells = new Array(this.rows);
+        this.cells = make2DArray(this.rows, this.cols);
 
         for(let i = 0; i < this.rows; i++){
-            this.cells[i] = new Array(this.cols);
-        }
-
-        for(let i = 0; i < rows; i++){
             this.neighbors[i] = {};
-            for(let j = 0; j < cols; j++){
+            for(let j = 0; j < this.cols; j++){
                 this.neighbors[i][j] = [];
                 for (let k = -1; k < 2; k++) {
                     for (let l = -1; l < 2; l++) {
-                        let row = (k + i + rows) % rows;
-                        let col = (l + j + cols) % cols;
+                        let row = (k + i + this.rows) % this.rows;
+                        let col = (l + j + this.cols) % this.cols;
                         this.neighbors[i][j].push({x : row, y : col});
                     }
                 }
@@ -215,32 +213,7 @@ export const reducer = (state, action) => {
 
     if(action.type === "CHANGE_DIM"){
 
-        console.log("Changing dimensions");
-
-        // state.recalculate(action.data.increase, action.data.rows, action.data.cols);
         state.recalculate(action.data);
-
-        // if(rows > state.maxRows){
-        //     rows = state.maxRows;
-        // }
-
-        // if(rows < state.minRows){
-        //     rows = state.minRows;
-        // }
-
-        // if(cols > state.maxCols){
-        //     cols = state.maxCols;
-        // }
-
-        // if(cols < state.minCols){
-        //     cols = state.minCols;
-        // }
-
-        // state.rows = rows;
-        // state.cols = cols;
-
-        // state.cells = make2DArray(rows, cols);
-        // StateDict[StateNames[state.currentState]](state);
 
     }else if(action.type === "TOGGLE_ON"){
 
@@ -283,10 +256,6 @@ export const reducer = (state, action) => {
             }
         }
     
-    }else if(action.type === "SET_MAX_DIM"){
-
-        return new State(null, action.data.rows, action.data.cols);
-
     }else if(action.type === "SIMULATING"){
 
         console.log("SIM....");
